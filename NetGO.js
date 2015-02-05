ab659578b1a83d86c0c14223856e3f2ecbf6896d
root <mteter13@gmail.com>
2015-02-04 20:26:54 -0500

    draws board

--- NetGO.js
+++ NetGO.js
@@ -2,7 +2,19 @@ if (Meteor.isClient) {
   $('document').ready(function() {
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
-  
+    context.lineWidth = 2;
+    
+    for (var i=0; i < 19; i++) {
+      context.beginPath();
+      context.moveTo(i*40 + 20, 20);
+      context.lineTo(i*40 + 20, 740);
+      context.stroke();
+      
+      context.beginPath();
+      context.moveTo(20, i*40 + 20);
+      context.lineTo(740, i*40 + 20);
+      context.stroke();
+    }
     
   });
   
