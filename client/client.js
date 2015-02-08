1ec12d04e0460072db5f185f6b79f385de025721
Drew Teter <mteter13@gmail.com>
2015-02-08 15:13:01 -0500

    debugging check

--- client/client.js
+++ client/client.js
@@ -35,8 +35,9 @@ $('document').ready(function() {
     if (tempGame.position[x][y].status == "empty") {
       tempGame.position[x][y].status = turn(tempGame.moveList);
       tempGame.moveList.push(tempGame.position[x][y]);
-      console.log(tempGame);
       Games.update(tempGame._id, {$set: {position: tempGame.position, moveList: tempGame.moveList}});
+    } else {
+      console.log("You can't go there.")
     }
     
   });
