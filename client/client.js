cdc8709426e33a2063deaf342dc457060db71626
Drew Teter <mteter13@gmail.com>
2015-02-22 00:25:16 -0500

    misc

--- client/client.js
+++ client/client.js
@@ -6,28 +6,28 @@ goban = new Board();
 var gobanDep = new Deps.Dependency();
 
 Template.info.helpers({
-  turn: function() { 
-    gobanDep.depend();
-    return goban.turn(); 
-  }
+    turn: function() { 
+        gobanDep.depend();
+        return goban.turn(); 
+    }
 });
 
 $('document').ready(function() {
-  
-  var renderer = new Renderer(document.getElementById("canvas"));
-  
-  Tracker.autorun(function() {
-    renderer.renderBoard();
-    renderer.drawPosition(goban.fromJson(game.fetch()[0]));
-  });
-  
-  renderer.canvas.addEventListener("mousedown", function(e) {
-    var rect = this.getBoundingClientRect();
-    var row = Math.floor((e.clientY - rect.top)/40);
-    var column = Math.floor((e.clientX - rect.left)/40);
-    goban.addMove(row, column);
-    Games.update(goban._id, {$set: goban.toJson()});
-    gobanDep.changed();
-  });
-  
+    
+    var renderer = new Renderer(document.getElementById("canvas"));
+    
+    Tracker.autorun(function() {
+        renderer.renderBoard();
+        renderer.drawPosition(goban.fromJson(game.fetch()[0]));
+    });
+    
+    renderer.canvas.addEventListener("mousedown", function(e) {
+        var rect = this.getBoundingClientRect();
+        var row = Math.floor((e.clientY - rect.top)/40);
+        var column = Math.floor((e.clientX - rect.left)/40);
+        goban.addMove(row, column);
+        Games.update(goban._id, {$set: goban.toJson()});
+        gobanDep.changed();
+    });
+    
 });
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 var Games = new Meteor.Collection("games");
 
 Meteor.startup(function () {
-  Games.remove({});
-  if (Games.find({}).count() == 0) {
-    var game = new Board();
-    Games.insert(game.toJson());
-  }
+    Games.remove({});
+    if (Games.find({}).count() == 0) {
+        var game = new Board();
+        Games.insert(game.toJson());
+    }
 });
 
 Meteor.publish("games", function() {
-  return Games.find();
+    return Games.find();
 });
